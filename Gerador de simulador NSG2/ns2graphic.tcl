cb9c841b10ffba9942ff710a9e7939c57ac1ec0d
Jhonathan Lobato <jhonathanlobato20@gmail.com>
2022-06-08 15:51:34 -0300

    Gerador TCL

---
+++ Gerador de simulador NSG2/ns2graphic.tcl
@@ -0,0 +1,83 @@
+# This script is created by NSG2 beta1
+# <http://wushoupong.googlepages.com/nsg>
+
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(stop)   10.0                         ;# time of simulation end
+
+#===================================
+#	Colors
+#===================================
+
+$ns color 1 Blue
+$ns color 2 Red
+$ns color 3 Green
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Open the NS trace file
+set tracefile [open out.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open out.nam w]
+$ns namtrace-all $namfile
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 5 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+
+#===================================
+#        Links Definition        
+#===================================
+#Createlinks between nodes
+$ns duplex-link $n0 $n3 100.0Mb 10ms DropTail
+$ns queue-limit $n0 $n3 50
+$ns duplex-link $n2 $n3 100.0Mb 10ms DropTail
+$ns queue-limit $n2 $n3 50
+$ns duplex-link $n4 $n3 100.0Mb 10ms DropTail
+$ns queue-limit $n4 $n3 50
+$ns duplex-link $n1 $n3 100.0Mb 10ms DropTail
+$ns queue-limit $n1 $n3 50
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n3 orient right-down
+$ns duplex-link-op $n2 $n3 orient right-up
+$ns duplex-link-op $n4 $n3 orient left-up
+$ns duplex-link-op $n1 $n3 orient left-down
+
+#===================================
+#        Agents Definition        
+#===================================
+
+#===================================
+#        Applications Definition        
+#===================================
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam out.nam &
+    exit 0
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
